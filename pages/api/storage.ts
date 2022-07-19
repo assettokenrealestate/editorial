bbefed85d186249dd5dbf53f85a52c892abd6562
German <gocardenasc@gmail.com>
2022-07-19 19:30:22 +0200

    get metadata

---
+++ pages/api/storage.ts
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { create } from "ipfs-core";
+
+export default async function store(req: NextApiRequest, res: NextApiResponse) {
+  const node = await create();
+  const metadata = node.cat(
+    "bafkreialalpon3l3t5pmgm7wy4icfswigwq4qwssjc5cmwbnk3bv3dqjqa"
+  );
+  const decoder = new TextDecoder();
+  let data = "";
+
+  for await (const chunk of metadata) {
+    data += decoder.decode(chunk, { stream: true });
+  }
+  res.status(200).json({metadata: data});
+}
