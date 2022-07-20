82ff48f76b9c4b722751751727e175c806b8a02e
German <gocardenasc@gmail.com>
2022-07-20 16:06:38 +0200

    retrive hq image

---
+++ pages/nfthq.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next"
+import Image from "next/image"
+
+const NftHQ: NextPage = () => {
+    return <Image src="https://nftstorage.link/ipfs/bafybeiflju5nq6wzmv6pihqwx3p6jawvzrqxytvddkozzzxvizqsjm6ll4/wetransfer_creacio-generalitat-cara-nft_hq-png_2022-06-23_0949/Creacio%CC%81%20Generalitat%20Cara%20(NFT_HQ).png" layout='fill' />
+}
+
+export default NftHQ
